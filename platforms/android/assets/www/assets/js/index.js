2bbbbf29e0e7415338d926b72cb9cabaff14d605
A-Garrido5 <adrian.garrido@usach.cl>
2015-10-21 11:37:21 -0300

    Implementando regiones, ciudades y comunas

--- platforms/android/assets/www/assets/js/index.js
+++ platforms/android/assets/www/assets/js/index.js
@@ -1,6 +1,6 @@
 
 
-localStorage.removeItem("username")
+
 
 $(document).ready(function (){
 
@@ -16,11 +16,28 @@ $(document).ready(function (){
 
     var ubicacionActual= window.location.pathname;
 
+    
+   //if(ubicacionActual!="login.html"){
 
-    if(ubicacionActual!="/login.html"){
-    //if(ubicacionActual!="/android_asset/www/login.html"){
-      location.href="login.html";  
-  }
+    if(ubicacionActual==="/data/data/com.adobe.phonegap.app/files/files/phonegapdevapp/www/index.html"){
+
+    }
+
+    else if(ubicacionActual==="/data/data/com.adobe.phonegap.app/files/files/phonegapdevapp/www/login.html"){
+
+    }
+
+    else if(ubicacionActual==="/data/data/com.adobe.phonegap.app/files/files/phonegapdevapp/www/crearUsuario.html"){
+
+    }
+
+    else if(ubicacionActual!="/android_asset/www/login.html" && ubicacionActual!="/login.html" && ubicacionActual!="/crearUsuario.html" && ubicacionActual!="/android_asset/www/crearUsuario.html"){
+        
+        location.href="login.html";  
+    }
+
+
+  
 		
 		//$('#dialogLogin').show();
 		//$( "#dialogLogin" ).trigger( "click" );
@@ -54,7 +71,7 @@ function logout(){
 
   localStorage.removeItem("username");
 
-  location.href="/android_asset/www/login.html";
+  location.href="login.html";
 
 
 }
@@ -77,7 +94,7 @@ function login(datos){
                                   }
             else{
              // alert("            "+json.mensaje +'\n'+json.nombres);
-//                     localStorage.setItem("username", json.nombres);
+
 				
       				$('#nameRight').text(json.nombres);
       				localStorage.setItem("username", json.nombres);
@@ -123,23 +140,172 @@ $('#botonLogin').click(function() {
 
 });
 
-function enviar(){
 
-  //location.href="login.html"
+function crearCuenta(){
+  location.href="crearUsuario.html";
+}
 
-  //$('element_to_pop_up').bPopup();
+function crearUsuario(nombre, apellidos, mail, telefono, celular, direccion, idPais){
+
+  var urlCreate ="";
+
+  $.ajax({
+          url: urlCreate,
+          type: "GET",
+          dataType: "json",
+          success: function(json) {
+            alert(JSON.stringify(json));
+
+               
+      
+            
+          },
+          error:function (xhr, ajaxOptions, thrownError) {
+             alert(JSON.stringify(thrownError));
+             alert(JSON.stringify(xhr));
+          }
+    });
+
+
+
+}
+
+function obtenerRegiones(){
+  alert("Obteniendo regiones");
+
+  var urlGetRegion ="http://sae1.imatronix.com:2614/WEBAPI_SERVICE/api/Region";
+
+  $.ajax({
+          url: urlGetRegion,
+          type: "GET",
+          dataType: "json",
+          success: function(json) {
+            alert(JSON.stringify(json));
+
+            if (json != "") {
+
+               var selectObject = $('#region');
+
+               var jsonObject = eval(json);
+
+               alert(jsonObject[0].glosa);
+
+
+                
+
+               for (var n = 0; n < jsonObject.length; n++) {
+                    selectObject.append(new Option(jsonObject[n].glosa, jsonObject[n].glosa.Value));
+               };
+            }
+
+               
+      
+            
+          },
+          error:function (xhr, ajaxOptions, thrownError) {
+             alert(JSON.stringify(thrownError));
+             alert(JSON.stringify(xhr));
+          }
+    });
+}
+
+
+function validarCampos(){
+
+  var expr = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+  var filter6=/^[A-Za-z\_\-\.\s\xF1\xD1]+$/;
+  var num = /^([0-9])*$/;
+
+  var errores=0;
+
+
+  
+  if (!filter6.test(document.getElementById("userName").value)){
+    alert("Nombre de usuario inválido");
+    errores++;
+  }
+  
+  if (!filter6.test(document.getElementById("userLastName").value)){
+    alert("Apellido inválido");
+    errores++;
+  }
+  
+
+  if (!expr.test(document.getElementById("email").value)){
+    alert("Correo inválido");
+    errores++;
+  }
+
+  if(document.getElementById("pass").value.length<4){
+    alert("La contraseña debe ser mayor a 4 caracteres");
+    errores++;
+  }
+
+  if(document.getElementById("pass").value != document.getElementById("passConfirm").value){
+    alert("Las contraseñas no coinciden");
+    errores++;
+  }
+
+  if(!num.test(document.getElementById("mobileFono").value)){
+    alert("Número de celular inválido");
+    errores++;
+  }
+
+  if(errores>0){
+    alert("Errores: "+errores);
+    return false;  
+  }  
+
+  else{
+    return true;  
+  }
+
+  
 
 }
 
 
 
+
 $('#sendToCreate').click(function() {
 
+        var esValido=validarCampos();
 
-        alert("funciona JQuery");
+        var nombres = $("#userName").val();
+        var apellidos = $("#userLastName").val();
+        var mail = $("#email").val();
+        var contrasena = $("#pass").val();
+        var repetirPass = $("#passConfirm").val();
+        var telefono = $("#fono").val();
+        var celular = $("#mobileFono").val();
+        var direccion = $("#address").val();
+        var idPais = document.getElementById("idCountry");
+
+        var i = idPais.selectedIndex;
+
+        var realIdPais = idPais.options[i].text;
+
+        if(esValido){
+          alert("Todo bien");
+        }
+
+
+
         
 
+        
+
+        
+
+        
+        
+        
+
+        
+        //location.href="login.html"
 
 });
+
+
 
 var permanentStorage = window.localStorage;
